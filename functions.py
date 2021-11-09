44be0e7dc6c0534b657032268a9a9aa89c8d97c9
Luca <luca.romani98@gmail.com>
2021-11-09 15:47:33 +0100

    Added functions file with preprocess function

---
+++ functions.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import re
+import csv
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import pickle
+
+
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+
+
+
+def preprocess(df):
+
+    x = re.sub('[^a-zA-Z]', '', df)
+
+    lower = str.lower(df).split()
+
+
+    stop_words = set(stopwords.words('english'))
+    no_sw = [w for w in lower if not w in words]
+
+    lemmatizer = WordNetLemmatizer()
+
+    output = [lemmatizer.lemmatize(w) for w in no_sw]
+
+    return(' '.join(output))
