b5e6a1a54a9c1f7f94ec0ed87163f40e3ac34a49
Matteo Di Mauro <69036405+matteodimauro@users.noreply.github.com>
2021-11-09 17:22:12 +0100

    Update merge_tsv.py

--- merge_tsv.py
+++ merge_tsv.py
@@ -12,15 +12,22 @@ import random
 import time
 
 #merge all the .tsvs on each of the 383 folders
+col = ['animeTitle','animeType','animeNumEpisode','releaseDate','endDate','animeNumMembers','animeScore','animeUsers','animeRank','animePopularity','animeDescriptions','animeRelated','animeCharacters','animeVoices','animeStaff','Url']
+
+#merge all the .tsvs on each of the 383 folders
 path = r"C:\Users\matte\Documents\II Year\ADM\Homework3_ADM\html-20211105T160926Z-001"
 workpath = path + r"\tsv\page_"
-df = pd.DataFrame(columns = ['animeTitle','animeType','animeNumEpisode','releaseDate','endDate','animeNumMembers','animeScore','animeUsers','animeRank','animePopularity','animeDescriptions','animeRelated','animeCharacters','animeVoices','animeStaff','Url'])
+
 lista = []
+
 for i in range(383):
     if i == 285 or i == 292:
         i+=1
     all_files = glob.glob(os.path.join((workpath+str(i+1)), "*.tsv"))
     for file in all_files:
-        df.loc[len(df)] = file
-print(df.head())
+        #print(list(pd.read_csv(file, sep=',',nrows=1)))
+        lista.append(list(pd.read_csv(file, sep=',',nrows=1)))
+        #print(lista) 
+        #df.append(pd.read_csv(file, header = None, sep=','))
+df = pd.DataFrame(lista, columns = col)
 df.to_csv(path+"\merged.tsv")
