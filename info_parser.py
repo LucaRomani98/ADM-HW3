ba0f2067dc6e5608f3cea22dfe1e99af93532cc2
Matteo Di Mauro <69036405+matteodimauro@users.noreply.github.com>
2021-11-09 01:05:40 +0100

    Update info_parser.py
    Finished Part 1

--- info_parser.py
+++ info_parser.py
@@ -12,48 +12,148 @@ import csv
 
 def parse_info(html_dir, tsv_dir, index):
     with open(html_dir, encoding = 'utf-8') as fp:
-        soup = BeautifulSoup(fp, "html.parser")
+        page_soup = BeautifulSoup(fp, "html.parser")
 
-    animeTitle = soup.find('h1', class_='title-name h1_bold_none').text
-
-    finder = soup.find(text=re.compile('Type:'))
-    animeType = finder.parent.parent.text.split()[-1]
-
-    finder = soup.find(text=re.compile('Episodes:'))
-    animeNumEpisode = int(finder.parent.parent.text.split()[-1])
+    animeTitle = str(page_soup.find('meta', {'property': 'og:title'}).get('content'))
     
-    finder = soup.find('span', text=re.compile('Aired:').parent.text.split('\n')[2].strip().split(' to ')
-    releaseDate = dt.strptime(finder[0], '%b %d %Y')
+    animeType = ""
+    for t in page_soup.find_all('div', class_ = 'spaceit_pad'):
+        type_ = t.text.split()
+        if type_[0] == "Type:":
+            animeType = str(type_[1])
 
-    if len(finder) == 2:
-        endDate = dt.strptime(finder[1], '%b %d %Y')
-    else:
-        endDate = ''
-    
-    finder = soup.find(text=re.compile('Members '))
-    animeNumMembers = int(finder.parent.parent.text.split()[3].replace(',',''))
+    animeNumEpisode = 0
+    for t in page_soup.find_all('div', class_ ='spaceit_paid'):
+        ep = t.text.split()
+        if ep[0] == "Episodes:":
+            try:
+                animeNumEpisode = int(ep[1])
+            except ValueError:
+                animeNumEpisode = ''
 
-    animeScore = float(soup.find('span', itemprop = 'ratingValue').text)
+    releaseDate = ''
+    endDate = ''
+    for i in page_soup.find_all('div', class_ = 'spaceit_paid'):
+        date_content = i.text.split()
+        if date_content[0] == "Aired":
+            date = date_content[1:]
+            if "Not available" in " ".join(date) or "Not Available" in " ".join(date):
+                releaseDate, endDate = '', ''
+            dates_l = []
+            for sr in date:
+                t = re.findall(r'[a-zA-Z]{0,3}[0-9]{0,2}[0-9]{0,4}', sr)
+                dates_l.append(t[0])
+            dates_l = list(filter(None, dates_l))
+            f_l = dates_l
+            s_l = []
+            if 'to' in dates_l:
+                i = dates_l.index('to')
+                f_l = dates_l[i]
+                s_l = dates_l[i+1:]
+            f_counter = len(f_l)
+            s_counter = len(s_l)
+            if f_counter == 3:
+                releaseDate = " ".join(f_l)
+                releaseDate = dt.strptime(releaseDate, '%b %d %Y').date()
+            elif f_counter == 2:
+                releaseDate = " ".join(f_l)
+                releaseDate = dt.strptime(releaseDate, '%b %Y').date()
+            elif f_counter == 1:
+                releaseDate = dt.strptime(f_l[0], '%Y').date()
+            if s_counter == 3:
+                endDate = " ".join(s_l)
+                endDate = dt.strptime(endDate, '%b %d %Y').date()
+            elif s_counter == 2:
+                endDate = " ".join(s_l)
+                endDate = dt.strptime(endDate, '%b %Y').date()
+            elif s_counter == 1:
+                endDate = dt.strptime(s_l[0], '%Y').date()
+    try:
+        animeNumMembers = int(''.join(re.findall(r'\d+', page_soup.find('span', class_ = "numbers members").text)))
+    except ValueError:
+        animeNumMembers = ''
 
-    animeUsers = int(soup.find('span', itemprop = 'ratingCount').text)
+    try:
+        animeScore = float(page_soup.find('div', class_ = "fl-l score").find('div').text)
+    except ValueError:
+        animeScore = ''
 
-    finder = soup.find(text=re.compile('Members '))
-    if finder.parent.parent.text.replace('#','').replace('P',' ').split()[1].isnumeric():
-        animeRank = int(finder.parent.parent.text.replace('#','').replace('P',' ').split()[1])
-    else:
+    try:
+        animeUsers = int(''.join(re.findall(r'\d+', page_soup.find('div', class_ = "fl-l score").get('data-user'))))
+    except ValueError:
+        animeUsers = ''
+
+    try:
+        animeRank = int(re.findall(r'\d+', page_soup.find('span', class_ = 'numbers ranked').text)[0])
+    except IndexError:
         animeRank = ''
 
-    animePopularity = int(soup.find(text=re.compile('Popularity:')).parent.parent.text.replace('#','').split()[-1])
+    try:
+        animePopularity = int(re.findall(r'\d+', page_soup.find('span', class_ = 'numbers popularity').text)[0])
+    except ValueError:
+        animePopularity = ''
 
-    animeDescription = soup.find('p', itemprop = 'description').text
-
-    animeRelated = []
+    animeDescription = ""
+    synopsis = page_soup.find('p', intemprop = "description")
+    try:
+        if "No synopsis" not in synopsis.text:
+            if synopsis.find('span'):
+                s = synopsis.find('span')
+                animeDescription = synopsis.text.replace(s.text, '')
+            else :
+                animeDescription = synopsis.text
+        else:
+            animeDescription = ''
+    except:
+        animeDescription = ''
+    
+    try:
+        animeRelated = []
+        for i in page_soup.find('table', class_ = "anime_detail_related_anime").find_all('a'):
+            if i.text not in animeRelated:
+                animeRelated.append(i.text)
+        if len(animeRelated ) > 0:
+            animeRelated = animeRelated
+        else:
+            animeRelated = ''
+    except AttributeError:
+        animeRelated = ''
 
     animeCharacters = []
+    if len(page_soup.find_all('h3', class_ = "h3_characters_voice_actors")) == 0:
+        animeCharacters = ''
+    for i in page_soup.find_all('h3', class_ = "h3_characters_voice_actors"):
+        animeCharacters.append(i.text)
+    animeCharacters = list(dict.fromkeys(animeCharacters))
 
     animeVoices = []
+    if len(page_soup.find_all('td', class_ = "va-t ar pl4 pr4")) == 0:
+        animeVoices = ''
+    for i in page_soup.find_all('td', class_ = "va-t ar pl4 pr4"):
+        animeVoices.append(i.find('a').text)
+    animeVoices = list(dict.fromkeys(animeVoices))
 
     animeStaff = []
+    staff = page_soup.find_all('div', class_ = 'detail-characters-list clearfix')
+    if len(staff) == 1:
+        staff = staff[0]
+    elif len(staff) == 2:
+        staff = staff[1]
+    else:
+        animeStaff = ''
+        return
+    staff_s = []
+    for i in staff.find_all('a'):
+        if ' '.join(i.text.split()) != '':
+            staff_s.append(' '.join(i.text.split()))
+    staff_t = []
+    for i in staff.find_all('small'):
+        staff_t.append(i.text)
+    for i,j in zip(staff_s, staff_t):
+        animeStaff.append([i, j])
+
+
+    URL = page_soup.find_all('link')[0]['href']
 
     with open(tsv_dir, encoding = 'utd-8', 'w') as tsv:
         tsv.write(headers()+'\n \n')
@@ -62,7 +162,7 @@ def parse_info(html_dir, tsv_dir, index):
 
 
 def headers():
-    return "animeTitle\t animeType\t animeNumEpisode\t releaseDate\t endDate\t animeNumMembers\t animeScore\t animeUsers\t animeRank\t animePopularity\t animeDescriptions\t animeRelated\t animeCharacters\t animeVoices\t animeStaff"
+    return "animeTitle\t animeType\t animeNumEpisode\t releaseDate\t endDate\t animeNumMembers\t animeScore\t animeUsers\t animeRank\t animePopularity\t animeDescriptions\t animeRelated\t animeCharacters\t animeVoices\t animeStaff\t URL"
 
 
 
