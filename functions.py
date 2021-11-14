5d1b3db86e7d6f1b1431566787ecc06b2699ce35
Matteo Di Mauro <69036405+matteodimauro@users.noreply.github.com>
2021-11-14 14:54:06 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -245,25 +245,18 @@ def parse_pages():
         parse_info(directory, completeName, index)
         index+=1
 
-
-#merge all the .tsvs on each of the 383 folders
-col = ['animeTitle','animeType','animeNumEpisode','releaseDate','endDate','animeNumMembers','animeScore','animeUsers','animeRank','animePopularity','animeDescriptions','animeRelated','animeCharacters','animeVoices','animeStaff','Url']
-
-#merge all the .tsvs on each of the 383 folders
-workpath = r"tsv\page_"
-
-lista = []
-
-for i in range(383):
-    all_files = glob.glob(os.path.join((workpath+str(i+1)), "*.tsv"))
-    for file in all_files:
-        #print(list(pd.read_csv(file, sep=',',nrows=1)))
-        lista.append(list(pd.read_csv(file, sep='\t',nrows=1)))
-        #print(lista) 
-        #df.append(pd.read_csv(file, header = None, sep=','))
-    print(i)
-df = pd.DataFrame(lista, columns = col)
-df.to_csv("merged.tsv", sep='\t')
+def merging():
+    #merge all the .tsvs on each of the 383 folders
+    col = ['animeTitle','animeType','animeNumEpisode','releaseDate','endDate','animeNumMembers','animeScore','animeUsers','animeRank','animePopularity','animeDescriptions','animeRelated','animeCharacters','animeVoices','animeStaff','Url']
+    workpath = r'tsv\page_'
+    lista = []
+    for i in range(383):
+        all_files = glob.glob(os.path.join((workpath+str(i+1)), "*.tsv"))
+        for file in all_files:
+            lista.append(list(pd.read_csv(file, sep='\t',nrows=1)))
+            print(i)
+    df = pd.DataFrame(lista, columns = col)
+    df.to_csv(path+r'\merged.tsv', sep='\t')
 
 
 def handle():
